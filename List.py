bf7f0078a7aa8262c586497d535073bfc3fab598
Lolo Almuqbil <119347835+Lolo-mg@users.noreply.github.com>
2023-01-12 11:00:09 +0300

    Add files via upload

---
+++ List.py
@@ -0,0 +1,13 @@
+#q1
+list1 =  [5, 4, 17, 19, 30, 2, 7, 10, 45]
+Sum = sum(list1)
+print(Sum)
+#q2
+Max = max(list1)
+print(Max)
+#q3
+new_list = [x for x in list1 if x % 2 == 0]
+print(new_list)
+#q4
+print(list1[:5])
+
